483ae2cf9d98bdbea85651aa5275d5faddce0a13
Mehrab Rahman <mehrab.rahman@revature.com>
2026-02-20 11:30:03 -0600

    removing old files

--- pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "chunking data hitch3.txt"
-cat hitch3.txt | python chonk.py
-echo "Created partitions"
-ls part* | xargs -n 1 -P 4 cat | python mapper.py | sort | python reducer.py > out
-echo "Output generated in out"
-#less out
--- testing.py
+++
@@ -1,7 +0,0 @@
-import exmath
-
-def test_add():
-    assert exmath.add(1, 1) == 2
-
-def test_sub():
-    assert exmath.sub(2,1) == 1
